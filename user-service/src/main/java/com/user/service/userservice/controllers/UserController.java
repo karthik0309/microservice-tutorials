361beee2160726c989cabea5bdd4742e641c04f4
Karthik Belida <Karthik392001@gmail.com>
2023-06-03 21:53:36 +0530

    Made user service fault tolerant

--- user-service/src/main/java/com/user/service/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/user/service/userservice/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.user.service.userservice.controllers;
 import com.user.service.userservice.models.Rating;
 import com.user.service.userservice.models.User;
 import com.user.service.userservice.service.user.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
         User user1= userService.createUser(user);
@@ -24,11 +28,22 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserById(@PathVariable String userId){
         User user = userService.getUserById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception exception){
+        LOGGER.info("Rating hotel fallback has been executed");
+        User user = User.builder()
+                .name("DUMMY")
+                .about("Service down because of: "+exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> users = userService.getAllUsers();
